6df6e0942a302d265a6bfe15ec77d2add69d9061
Siulhk <siullhk2@gmail.com>
2024-09-02 22:24:18 -0300

    Create exercicios.py

---
+++ exercicios.py
@@ -0,0 +1,46 @@
+1.  Linha 2:
+    "a" imprimira: 1
+    "b" imprimira: 2
+    "c" imprimira: 3
+
+    Linha 6:
+    "a" imprimira: 101
+    "b" imprimira: 102
+    "c" imprimira: 103
+
+    Linha 12:
+    "a" imprimira: 1
+    "b" imprimira: 2
+    "c" imprimira: 3
+
+2.  Linha 2:
+    "n1" imprimira: 1
+    "n2" imprimira: 2
+    "n3" imprimira: 3
+
+    Linha 6:
+    "n1" imprimira: 101
+    "n2" imprimira: 102
+    "n3" imprimira: 103
+
+    Linha 12:
+    "n1" imprimira: 101
+    "n2" imprimira: 102
+    "n3" imprimira: 103
+
+3.  Ambas as linhas imprimirão o número 5.
+
+4.  As linhas 11, 13 e 14 imprimirão o número 6;
+    A linha 16 imprimira 0.
+
+5.  Ambas imprimirão o número 11.
+
+6.  As linhas 4 e 11 imprimirão o número 8;
+    A linha 5 imprimira o número "7" e a linha 12 imprimira o número "2".
+
+7.  Se "a" for maior que "b" então o número a ser impresso será "0",
+mas se "b" for maior que "a" então o número impresso será uma adição de a+b(ex: a = 2, b = 3, s = 5)
+
+    b) Sim é possivel, o valor lido será o mesmo adiquirido com "scanf("%f", pz);", se
+    um ponteiro adiquire um novo valor, então a variavel a qual o mesmo está referenciando também
+    adiquire o mesmo valor.
